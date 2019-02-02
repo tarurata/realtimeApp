e7f62f5882a13a0973ad00c6170bacaa6fa80493
tarurata <saycheese5963@yahoo.co.jp>
2019-02-02 15:38:53 +0900

    Sat Feb  2 15:38:53 JST 2019 Created Migration, Model, Controller and Factories

--- database/migrations/2019_02_02_035039_create_replies_table.php
+++ database/migrations/2019_02_02_035039_create_replies_table.php
@@ -15,6 +15,14 @@ class CreateRepliesTable extends Migration
     {
         Schema::create('replies', function (Blueprint $table) {
             $table->increments('id');
+            $table->text('body');
+
+            $table->integer('quesetion_id')->unsigned();
+
+            $table->increments('user_id');
+
+
+            $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade');
             $table->timestamps();
         });
     }
