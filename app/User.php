00c40005f0525dc2c7156431f0873ec1374e3113
tarurata <saycheese5963@yahoo.co.jp>
2019-02-09 21:39:24 +0900

    JWT Package

--- app/User.php
+++ app/User.php
@@ -3,10 +3,11 @@
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -32,5 +33,25 @@ class User extends Authenticatable
         return $this->hasMany(Question::class);
     }
 
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+
+    public function getJWTIdentifier(){
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims(){
+        return [];
+    }
 
 }
+
+
--- routes/api.php
+++ routes/api.php
@@ -12,14 +12,30 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
 #Route::middleware('auth:api')->get('/user', function (Request $request) {
 #    return $request->user();
 #});
 #
 
 Route::apiResource('/question', 'QuestionController');
+
 Route::apiResource('/category', 'CategoryController');
+
 Route::apiResource('/question/{question}/reply', 'ReplyController');
+
 Route::post('/like/{reply}', 'LikeController@likeIt');
 Route::delete('/like/{reply}', 'LikeController@unlikeIt');
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+//], function ($router){
+], function (){
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+    Route::post('payload', 'AuthController@payload');
+});
